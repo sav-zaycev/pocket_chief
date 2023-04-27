19372a21724935b16fd0b48ba7c1e994bfdb9e42
Saveliy_Zaycev <sav.zaycev@gmail.com>
2023-04-27 15:46:42 +0300

    add login fragment front part

--- app/src/main/java/ru/zaycev/pocketchief/view/LoginFragment.kt
+++ app/src/main/java/ru/zaycev/pocketchief/view/LoginFragment.kt
@@ -1,17 +1,51 @@
 package ru.zaycev.pocketchief.view
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.text.TextUtils
+import android.util.Patterns
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.res.ResourcesCompat
+import androidx.core.widget.addTextChangedListener
+import androidx.fragment.app.Fragment
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 import ru.zaycev.pocketchief.R
 
 class LoginFragment : Fragment() {
+    private lateinit var loginEdit: TextInputEditText
+    private lateinit var loginInputLayout: TextInputLayout
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_login, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        loginEdit = requireView().findViewById(R.id.loginEditEmail)
+        loginInputLayout = requireView().findViewById(R.id.loginLayoutEmail)
+
+        loginEdit.addTextChangedListener {
+            val login: String = loginEdit.text.toString()
+
+            loginInputLayout.endIconDrawable =
+                if (login.isEmpty()) {
+                    ResourcesCompat.getDrawable(resources, R.drawable.bg_transparent, null)
+                } else if (isEmailValid(login)) {
+                    ResourcesCompat.getDrawable(resources, R.drawable.ic_check, null)
+                } else {
+                    ResourcesCompat.getDrawable(resources, R.drawable.ic_warning, null)
+                }
+        }
+    }
+
+    private fun isEmailValid(email: CharSequence): Boolean {
+        return Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    }
 }
+
